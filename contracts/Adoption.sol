a8bc79779b53ef40ab1d0a21c144b780de24a40b
rajeshrv <rajeshr_v@hotmail.com>
2018-01-26 09:17:18 +0400

    commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+
+address[3] public adopters;
+// Adopting a pet
+function adopt(uint petId) public returns (uint) {
+  require(petId >= 0 && petId <= 2);
+
+  adopters[petId] = msg.sender;
+
+  return petId;
+}
+
+// Retrieving the adopters
+function getAdopters() public view returns (address[3]) {
+  return adopters;
+}
+
+
+}
